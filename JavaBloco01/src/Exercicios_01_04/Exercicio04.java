fcd4dad1460eb9f7a05bb87ec0e7d275c1188c5f
Benior <benior.santos@gmail.com>
2022-04-01 19:04:59 -0300

    Exercícios feitos em java

---
+++ JavaBloco01/src/Exercicios_01_04/Exercicio04.java
@@ -0,0 +1,27 @@
+package Exercicios_01_04;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int x;
+		
+		System.out.print("Escreva um número: ");
+		x=ler.nextInt();
+		
+		if ((x%2)==0)
+		{
+			System.out.println("O numero é par e a raiz quadrada de "+x+" é "+Math.sqrt(x));
+		}
+		else 
+		{
+			System.out.print("O numero é ímpar e o numero "+x+" ao quadrado é "+Math.pow(x, 2));
+		}
+		
+
+	}
+
+}
